d51b0c5bf9bdd21a567e40d7037462f8b7e5d0d3
gusvicuna <gpvicuna@miuandes.cl>
2019-04-10 10:32:16 -0400

    Basic controllers done. Todo: tickets

--- app/controllers/ticket_types_controller.rb
+++ app/controllers/ticket_types_controller.rb
@@ -1,22 +1,37 @@
 class TicketTypesController < ApplicationController
   def index
+    @ticket_types = TicketType.all
+    render json: @ticket_types
   end
 
   def show
+    @ticket_type = TicketType.find_by_id(params[:id])
+    render json: @ticket_type
   end
 
   def new
+    @ticket_type = TicketType.new
   end
 
   def edit
   end
 
   def create
+    @ticket_type = TicketType.new(params)
   end
 
   def update
+    respond_to do |format|
+      if @ticket_type.update(params)
+        render json: @ticket_type
+      else
+        format.json { render json: @ticket_type.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @ticket_type.destroy
+    render json: @users
   end
 end
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,22 +1,37 @@
 class TicketsController < ApplicationController
   def index
+    @tickets = Ticket.all
+    render json: @tickets
   end
 
   def show
+    @ticket = Ticket.find_by_id(params[:id])
+    render json: @ticket
   end
 
   def new
+    @ticket = Ticket.new
   end
 
   def edit
   end
 
   def create
+    @ticket = Ticket.new(params)
   end
 
   def update
+    respond_to do |format|
+      if @ticket.update(params)
+        render json: @ticket
+      else
+        format.json { render json: @ticket.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @ticket.destroy
+    render json: @ticket
   end
 end
