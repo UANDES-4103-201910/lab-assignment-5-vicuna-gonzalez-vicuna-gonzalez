934bdf20a607cda1b55aedc663b0c687669d8c2f
gusvicuna <gpvicuna@miuandes.cl>
2019-04-10 09:44:56 -0400

    resources routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,7 @@
 Rails.application.routes.draw do
-  get 'event_venues/index'
-  get 'event_venues/show'
-  get 'event_venues/new'
-  get 'event_venues/edit'
-  get 'event_venues/create'
-  get 'event_venues/update'
-  get 'event_venues/destroy'
-  get 'events/index'
-  get 'events/show'
-  get 'events/new'
-  get 'events/edit'
-  get 'events/create'
-  get 'events/update'
-  get 'events/destroy'
-  get 'ticket_types/index'
-  get 'ticket_types/show'
-  get 'ticket_types/new'
-  get 'ticket_types/edit'
-  get 'ticket_types/create'
-  get 'ticket_types/update'
-  get 'ticket_types/destroy'
-  get 'tickets/index'
-  get 'tickets/show'
-  get 'tickets/new'
-  get 'tickets/edit'
-  get 'tickets/create'
-  get 'tickets/update'
-  get 'tickets/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/create'
-  get 'users/update'
-  get 'users/destroy'
+  resources :event_venues, :users, :events
+  resources :tickets do
+    resources :ticket_types
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
