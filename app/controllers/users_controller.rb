830824d08d2a4d7fdca81e38639a2b735e745445
gusvicuna <gpvicuna@miuandes.cl>
2019-04-10 10:18:22 -0400

    Free stylo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,19 +10,28 @@ class UsersController < ApplicationController
   end
 
   def new
-    @comment = Comment.new
+    @user = User.new
   end
 
   def edit
   end
 
   def create
-    @comment = Comment.new(params)
+    @user = User.new(params)
   end
 
   def update
+    respond_to do |format|
+      if @user.update(params)
+        render json: @user
+      else
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @user.destroy
+    render json: @users
   end
 end
