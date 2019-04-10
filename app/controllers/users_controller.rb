90bec2e1418e412e722b36a21ec9986cf3d1b05c
gusvicuna <gpvicuna@miuandes.cl>
2019-04-10 09:58:52 -0400

    Opa Gangnam

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,23 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
+    render json: @users
   end
 
   def show
+    @user = User.find_by_id(params[:id])
+    render json: @user
   end
 
   def new
+    @comment = Comment.new
   end
 
   def edit
   end
 
   def create
+    @comment = Comment.new(params)
   end
 
   def update
